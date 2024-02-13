6ac4174b71616990042b0c2ff1960dec1b173764
Swetharaji2 <148094551+Swetharaji2@users.noreply.github.com>
2024-02-13 12:47:33 +0530

    Update start_container.sh
    new commit

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull swetharaji/simple-python-flask-app
+docker pull swetharaji/simple-python-flask-app 
 
 # Run the Docker image as a container
 docker run -d -p 5000:5000 swetharaji/simple-python-flask-app
